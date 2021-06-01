64f3832e234a6d25138b830af8fc7ca848be7e20
Leonard Buechner <lbuechner96@gmail.com>
2021-06-01 18:18:26 +0200

    subscriber not working

---
+++ rrbot_description/nodes/pub_sub_gyro.py
@@ -0,0 +1,26 @@
+
+import rospy
+import numpy as np
+from geometry_msgs.msg import Vector3, Twist
+
+def measurementfunction(msg):
+    angular = np.ndarray([msg])
+
+def callback(msg):
+    measurementfunction(msg)
+
+def talker():
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        hello_str = "hello world %s" % rospy.get_time()
+        rospy.loginfo(hello_str)
+        pub.publish(hello_str)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
--- setspeed.sh
+++ setspeed.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-speed=$1
+speed=300
 rostopic pub -1 /rrbot/motor0_velocity_controller/command std_msgs/Float64 "data: $speed" &
 rostopic pub -1 /rrbot/motor1_velocity_controller/command std_msgs/Float64 "data: $speed" &
 rostopic pub -1 /rrbot/motor2_velocity_controller/command std_msgs/Float64 "data: $speed" &
